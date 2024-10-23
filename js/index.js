ba1e8a552ac9c78cc90a35e630f18c8eec5ea2e6
Cheyenne Kirby <cmk7@usca.edu>
2024-10-23 16:32:47 -0400

    I got my javascript to work imtotallynerdingout

--- js/index.js
+++ js/index.js
@@ -21,25 +21,94 @@ function myFunction2() {
      }
     }
 
-    function myFunction() {
-        var x = document.getElementById("click");
-        if (x.style.visibility === "hidden") {
-         x.style.visibility="visible";
-         x.style.display = "inline-block";
-         x.style.backgroundImage = "radial-gradient(black, midnightblue, rgb(81, 2, 113))";
-         x.style.border = "2px ridge white";
-         x.style.width = "570px";
-         x.style.height = "33vh";
-         x.style.overflow="scroll";
-         x.style.overflowX="hidden";
-         x.style.opacity = "1";
-         x.style.position="absolute";
-         x.style.top="20%";
-         x.style.left="30%";
-         x.style.borderRadius="10px";
-         x.style.color="aliceblue";
-         x.style.textShadow="0 0 3px midnightblue";
-         } else {
-          x.style.visibility = "hidden";
-         }
-        }
+function myFunction() {
+    var x = document.getElementById("click");
+    if (x.style.visibility === "hidden") {
+     x.style.visibility="visible";
+     x.style.display = "inline-block";
+     x.style.backgroundImage = "radial-gradient(black, midnightblue, rgb(81, 2, 113))";
+     x.style.border = "2px ridge white";
+     x.style.width = "570px";
+     x.style.height = "33vh";
+     x.style.overflow="scroll";
+     x.style.overflowX="hidden";
+     x.style.opacity = "1";
+     x.style.position="absolute";
+     x.style.top="20%";
+     x.style.left="30%";
+     x.style.borderRadius="10px";
+     x.style.color="aliceblue";
+     x.style.textShadow="0 0 3px midnightblue";
+     } else {
+      x.style.visibility = "hidden";
+     }
+    }
+function myFunction3() {
+    var x = document.getElementById("click3");
+    if (x.style.visibility === "hidden") {
+     x.style.visibility="visible";
+     x.style.display = "inline-block";
+     x.style.backgroundImage = "radial-gradient(black, midnightblue, rgb(81, 2, 113))";
+     x.style.border = "2px ridge white";
+     x.style.width = "25%";
+     x.style.height = "30%";
+     x.style.overflow="scroll";
+     x.style.overflow="hidden";
+     x.style.opacity = "1";
+     x.style.position="absolute";
+     x.style.top="20%";
+     x.style.left="37%";
+     x.style.borderRadius="10px";
+     x.style.color="aliceblue";
+     x.style.textShadow="0 0 3px midnightblue";
+     x.style.textAlign="center";
+     } else {
+      x.style.visibility = "hidden";
+     }
+    }
+function myFunction4() {
+    var x = document.getElementById("click4");
+    if (x.style.visibility === "hidden") {
+     x.style.visibility="visible";
+     x.style.display = "inline-block";
+     x.style.backgroundImage = "radial-gradient(black, midnightblue, rgb(81, 2, 113))";
+     x.style.border = "2px ridge white";
+     x.style.width = "25%";
+     x.style.height = "30%";
+     x.style.overflow="scroll";
+     x.style.overflow="hidden";
+     x.style.opacity = "1";
+     x.style.position="absolute";
+     x.style.top="20%";
+     x.style.left="37%";
+     x.style.borderRadius="10px";
+     x.style.color="aliceblue";
+     x.style.textShadow="0 0 3px midnightblue";
+     x.style.textAlign="center";
+     } else {
+      x.style.visibility = "hidden";
+     }
+    }
+function myFunction5() {
+    var x = document.getElementById("click5");
+    if (x.style.visibility === "hidden") {
+     x.style.visibility="visible";
+     x.style.display = "inline-block";
+     x.style.backgroundImage = "radial-gradient(black, midnightblue, rgb(81, 2, 113))";
+     x.style.border = "2px ridge white";
+     x.style.width = "25%";
+     x.style.height = "30%";
+     x.style.overflow="scroll";
+     x.style.overflow="hidden";
+     x.style.opacity = "1";
+     x.style.position="absolute";
+     x.style.top="20%";
+     x.style.left="37%";
+     x.style.borderRadius="10px";
+     x.style.color="aliceblue";
+     x.style.textShadow="0 0 3px midnightblue";
+     x.style.textAlign="center";
+     } else {
+      x.style.visibility = "hidden";
+     }
+    }
