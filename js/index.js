4e308f28d07eef65e191288360452e3ea87e23d9
Cheyenne Kirby <cmk7@usca.edu>
2024-10-23 23:48:19 -0400

    fun

--- js/index.js
+++ js/index.js
@@ -118,3 +118,28 @@ function myFunction5() {
       x.style.visibility = "hidden";
      }
     }
+
+    function myFunction6() {
+        var x = document.getElementById("click7");
+        if (x.style.visibility === "hidden") {
+         x.style.visibility="visible";
+         x.style.display = "inline-block";
+         x.style.backgroundImage = "radial-gradient(black, midnightblue, rgb(81, 2, 113))";
+         x.style.border = "2px ridge white";
+         x.style.width = "570px";
+         x.style.height = "33vh";
+         x.style.zIndex="1";
+         x.style.overflow="scroll";
+         x.style.overflowX="hidden";
+         x.style.opacity = "1";
+         x.style.position="absolute";
+         x.style.top="20%";
+         x.style.left="30%";
+         x.style.borderRadius="10px";
+         x.style.color="aliceblue";
+         x.style.textShadow="0 0 3px midnightblue";
+         x.style.boxShadow="-7px 7px 3px black";
+         } else {
+          x.style.visibility = "hidden";
+         }
+        }
